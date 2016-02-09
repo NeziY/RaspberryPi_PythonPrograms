023cc4a5d6b5f959f374f283984f996021315c82
NeziY <nezi62094@hotmail.com>
2016-02-09 16:33:53 +0200

    Create button_led.py

---
+++ button_led.py
@@ -0,0 +1,26 @@
+import RPi.GPIO as GPIO
+import time
+
+def main():
+
+    try:
+        GPIO.setupmode(GPIO.BCM)
+      
+        led_pin = 18
+        but_pin = 17
+        GPIO.setup(led_pin, GPIO.OUT)
+        GPIO.setup(but_pin, GPIO.IN, pull_up_down=GPIO.PUD_UP) # Button pin set as input w/ pull-up
+        GPIO.output(led_pin, GPIO.LOW)
+
+        while True:
+            if GPIO.input(but_pin): # button is released
+                GPIO.output(led_pin, GPIO.LOW)
+            else:
+                GPIO.outup(led_pin, GPIO.HIGH)
+                time.sleep(1)
+                GPIO.output(led_pin, GPIO.LOW)
+                time.sleep(1)
+    except KeyboardInterrupt: # If CTRL+C is pressed, exit cleanly:
+        GPIO.cleanup()
+
+main()
